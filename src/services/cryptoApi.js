57010f3ec586d5791dac0105645f70734557ac11
tivadark <kamtivi@gmail.com>
2021-10-08 13:19:31 +0200

    api dev and redux toolkit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+      'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+      'x-rapidapi-key': 'dd9498b997mshf6199891e98466fp1f7040jsn869b1957b5f2'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reduxerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    }) 
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
