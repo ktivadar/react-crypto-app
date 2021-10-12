3553fb0fcaf35a5c0759dd02f83428dd07c667f4
tivadark <kamtivi@gmail.com>
2021-10-12 21:12:04 +0200

    Add exchanges page and api

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -16,6 +16,9 @@ export const cryptoApi = createApi({
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins/?limit=${count}`)
         }),
+        getExchanges: builder.query({
+            query: () => createRequest('/exchanges'),
+          }),
         getCryptoDetails: builder.query({
             query: (coinId) => createRequest(`/coin/${coinId}`),
           }),
@@ -26,4 +29,4 @@ export const cryptoApi = createApi({
     }) 
 });
 
-export const {useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery} = cryptoApi;
+export const {useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery, useGetExchangesQuery} = cryptoApi;
