17a77b1faea6570b70906eb939837bc3a9cb83eb
tivadark <kamtivi@gmail.com>
2021-10-12 21:01:55 +0200

    Fixed navbar position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,30 +5,54 @@ import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOut
 import icon from '../images/cryptocurrency.png';
 
 const Navbar = () => {
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [screenSize, setScreenSize] = useState(null);
+
+    useEffect(()=> {
+        const handleResize = () => setScreenSize(window.innerWidth);
+
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    useEffect(() => {
+        if(screenSize <768) {
+            setActiveMenu(false);
+        } else {
+            setActiveMenu(true);
+        }
+    }, [screenSize])
+
     return (
         <div className="nav-container">
             <div className="logo-container">
                 <Avatar src={icon} size="large" />
                 <Typography.Title level={2} className="logo">
-                    <Link to="/">Cryptoverse</Link>
+                    <Link to="/">Cryptoinfo</Link>
                 </Typography.Title>
+                </div>
+                {activeMenu && (
                 <Menu theme="dark">
-                    <Menu.Item icon={<HomeOutlined/>}>
+                    <Menu.Item icon={<HomeOutlined />}>
                        <Link to="/">Home</Link> 
                     </Menu.Item>
-                    <Menu.Item icon={<FundOutlined/>}>
+                    <Menu.Item icon={<FundOutlined />}>
                        <Link to="/cryptocurrencies">Cryptocurrencies</Link> 
                     </Menu.Item>
-                    <Menu.Item icon={<MoneyCollectOutlined/>}>
+                    <Menu.Item icon={<MoneyCollectOutlined />}>
                        <Link to="/exchanges">Exchanges</Link> 
                     </Menu.Item>
-                    <Menu.Item icon={<BulbOutlined/>}>
+                    <Menu.Item icon={<BulbOutlined />}>
                        <Link to="/news">News</Link> 
                     </Menu.Item>
                 </Menu>
-            </div>
+
+                )}
         </div>
-    )
+    );
 }
 
 export default Navbar
